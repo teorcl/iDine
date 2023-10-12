be31da6cbe505847d77436104e62350cd3d84ae9
Teodoro Calle Lara <teodoro.calle@mergedev.co>
2023-10-11 21:40:46 -0500

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by ADMIN on 11/10/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
